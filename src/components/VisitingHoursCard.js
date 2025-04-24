a25a64364ba2eef73f770a8eb212b5ad542c3636
Tudor <tudor.subin@yahoo.com>
2025-04-24 20:38:16 +0300

    added zoo map

--- src/components/VisitingHoursCard.js
+++ src/components/VisitingHoursCard.js
@@ -4,37 +4,37 @@ import "./VisitingHoursCard.css";
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 
 function VisitingHoursCard() {
-    const[hours, setHours] = useState({
-        open_time: '',
-        close_time: '',
-    });
+  const[hours, setHours] = useState({
+      open_time: '',
+      close_time: '',
+  });
 
-    const fetchVisitingHours = async () => {
-        try {
-          const response = await axios.get(`http://localhost/ZooDashboard/zoo_dashboard/src/backend/getHours.php`);
-          //console.log(response);    //debug
-          if(Array.isArray(response.data)){
-            if(response.data.length>0 && typeof response.data[0] === 'object'){
-              setHours(response.data[0]);
-            }
-          } else {
-            console.error('Hours: expected array but received: ', response.data);
-          }
-        } catch (error) {
-          console.error('Error fetching hours:', error);
+  const fetchVisitingHours = async () => {
+    try {
+      const response = await axios.get(`http://localhost/ZooDashboard/zoo_dashboard/src/backend/getHours.php`);
+      //console.log(response);    //debug
+      if(Array.isArray(response.data)){
+        if(response.data.length>0 && typeof response.data[0] === 'object'){
+          setHours(response.data[0]);
         }
-    };
+      } else {
+        console.error('Hours: expected array but received: ', response.data);
+      }
+    } catch (error) {
+      console.error('Error fetching hours:', error);
+    }
+  };
 
-    useEffect(() => {
-        fetchVisitingHours();
-    }, []);
+  useEffect(() => {
+    fetchVisitingHours();
+  }, []);
 
-    return (
-        <div className="hours">
-            <h1><AccessTimeIcon/> Visiting Hours</h1>
-            <p>{hours.open_time} - {hours.close_time}</p>
-        </div>
-    );
+  return (
+    <div className="hours">
+      <h1><AccessTimeIcon/> Visiting Hours</h1>
+      <p>{hours.open_time} - {hours.close_time}</p>
+    </div>
+  );
 }
 
 export default VisitingHoursCard;
--- src/pages/map.js
+++ src/pages/map.js
@@ -3,45 +3,19 @@ import "./map.css";
 import "./shared.css";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import ZooMap from '../components/ZooMap';
 
 function MapPage() {
     
     return (
         <div className="page">
             <Navbar/>
-            <div className="banner location-banner"/>
-            <div className="content">
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-                <p>Hllow</p>
-            </div>
+                <div className="banner location-banner">
+                    <h1>Zoo Map</h1>
+                </div>
+                <div className="content">
+                    <ZooMap/>
+                </div>
             <Footer/>
         </div>
     );
