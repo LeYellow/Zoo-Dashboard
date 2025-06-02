f5a5f4bdbc643e153b65655d600b315a6f67812c
Tudor <tudor.subin@yahoo.com>
2025-06-02 18:55:10 +0300

    bug fixes

--- src/backend/addNewsArticleImage.php
+++ src/backend/addNewsArticleImage.php
@@ -5,12 +5,16 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
 $target_dir = "../../../extResources/News/";
-$target_file = $target_dir . basename($_FILES["image"]["name"]);
 
-if(move_uploaded_file($_FILES["image"]["tmp_name"],$target_file)) {
-    echo "Image uploaded succesfully!";
+if(isset($_FILES["image"]) && $_FILES["image"]["error"] === UPLOAD_ERR_OK) {
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    if(move_uploaded_file($_FILES["image"]["tmp_name"],$target_file)) {
+        echo "Image uploaded succesfully!";
+    } else {
+        http_response_code(500);
+        echo "Couldn't upload Article image";
+    }
 } else {
-    http_response_code(500);
-    echo "Couldn't upload Article image";
+    echo "No image uploaded";
 }
 ?>
--- src/backend/addNewsArticleText.php
+++ src/backend/addNewsArticleText.php
@@ -5,12 +5,16 @@ header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Ac
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
 $target_dir = "../../public/News/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
 
-if(move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)) {
-    echo "Text uploaded succesfully!";
+if(isset($_FILES["file"]) && $_FILES["file"]["error"] === UPLOAD_ERR_OK) {
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    if(move_uploaded_file($_FILES["file"]["tmp_name"],$target_file)) {
+        echo "Text uploaded succesfully!";
+    } else {
+        http_response_code(500);
+        echo "Couldn't upload Article text";
+    }
 } else {
-    http_response_code(500);
-    echo "Couldn't upload Article text";
+    echo "No text uploaded";
 }
 ?>
